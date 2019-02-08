e44f917cc47ccb3a44784550251d10755f5cfcf1
Kim Heidorn <heidorkn@gmail.com>
2019-02-08 20:22:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("skirts")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("skirts")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -23,13 +23,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[array.count-1]
 end
